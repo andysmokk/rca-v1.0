943a288a143938bdba9cebef8f9143dd28594af0
Andrii Konovalov <andy.smokk@gmail.com>
2024-01-10 17:09:30 +0100

    fix env

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -2,13 +2,28 @@ import { useState } from "react";
 import Image from "next/image";
 
 import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
 
 interface CarCardProps {
   car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-  return <div>CarCard</div>;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make} {model}
+        </h2>
+      </div>
+
+      <p>
+        <span></span>
+      </p>
+    </div>
+  );
 };
 
 export default CarCard;
