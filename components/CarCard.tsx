1c545eee9b6e58499dea7c639f1e4e2d9404ee2e
Andrii Konovalov <andy.smokk@gmail.com>
2024-01-15 21:35:00 +0100

    add CarDetails

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -67,18 +68,24 @@ const CarCard = ({ car }: CarCardProps) => {
             <Image src="/gas.svg" alt="gas" width={20} height={20} />
             <p className="text-[14px]">{city_mpg} MPG</p>
           </div>
+        </div>
 
-          <div className="car-card__button-container">
-            <CustomButton
-              title="View more"
-              containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
-              textStyles="text-white text-[14px] leading-[17px] font-bold"
-              rightIcon="/right-arrow.svg"
-              handleClick={() => setIsOpen(true)}
-            />
-          </div>
+        <div className="car-card__btn-container">
+          <CustomButton
+            title="View more"
+            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
         </div>
       </div>
+
+      <CarDetails
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
