ab70675d5ea122f9beb1a907aa94e3de7db7a7e2
Andrii Konovalov <andy.smokk@gmail.com>
2024-01-19 14:39:09 +0100

    fixed scroll when search params are updated

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -59,7 +59,7 @@ const SearchBar = () => {
       window.location.pathname
     }?${searchParams.toString()}`;
 
-    router.push(newPathname);
+    router.push(newPathname, { scroll: false });
   };
 
   return (
